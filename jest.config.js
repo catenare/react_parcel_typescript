182f3afc401ec4fb61069d19e0d5d102285fe45c
Johan Martin <johan@next45.co>
2020-05-02 15:41:32 +0200

    Add new rect file using parcel

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+
+  collectCoverageFrom: ["src/*.{js,ts}", "src/**/*.{js,ts}"],
+
+  coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+
+  coverageReporters: ["json", "text", "lcov", "clover"],
+
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+  testEnvironment: "jsdom",
+  transform: {
+    ".(ts|tsx)": "ts-jest",
+  },
+  testRegex: ["/test/.*\\.(test|spec)\\.(ts)$"],
+  coverageDirectory: "coverage",
+};
