078f038e6041aad1d695e2205a1984bc7e01de43
Johan Martin <johan@next45.co>
2020-05-02 16:04:49 +0200

    Basic react app with parcel as the bundler.

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+const name = "Johsn Perez";
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
